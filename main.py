8c24fc296aecc20c4ab729f511ce08978ba9886e
seolinjung <sj3182@nyu.edu>
2025-07-15 10:37:37 +0900

    minor changes to upload_db

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import argparse
 import os
-
 import pandas as pd
 
 from helper import extract_domain, retrieve_csv
-
 from validate_input import ValidateInput
 
 def apply_classification(row, mode="default"): 
@@ -13,7 +11,7 @@ def apply_classification(row, mode="default"):
 
     if mode=="seonhye":
         return validation.overwrite_seonhye()
-
+    
     if mode=="sales":
         return validation.overwrite_sales()
 
@@ -21,10 +19,10 @@ def apply_classification(row, mode="default"):
     
 def upload_db(args, db):
 
-    dest_path = os.path.join("data", args.date)
+    dest_path = os.path.join("data", "results", args.date)
 
     if not os.path.exists(dest_path):
-        os.mkdir(dest_path)
+        os.makedirs(dest_path, exist_ok=True)
 
     writer = pd.ExcelWriter(os.path.join(dest_path, "Sorted_DB.xlsx"))
     db.to_excel(writer)
@@ -37,17 +35,19 @@ def main(args):
     confirm_mail_path = retrieve_csv(args, "confirm_mail")
 
     # read main file 
-    main_df = pd.read_csv(retrieve_csv(args, "main"), usecols=["First Name", "Last Name", "Email", "Company (Custom)", "Title", "Related Record Owner"])
+    main_df = pd.read_csv(
+        retrieve_csv(args, "main"),
+        usecols=["First Name", "Last Name", "Email", "Company (Custom)", "Title", "Related Record Owner"])
+    
     # and make a copy of main
     main_df_copy = main_df.copy()
 
-    if sdr_confirm_path: # if exists
+    if sdr_confirm_path: 
         sdr_confirm_df = pd.read_csv(sdr_confirm_path)
         # merge based on commonality of emails 
         main_df_copy = main_df_copy.merge(sdr_confirm_df, on="Email", how="left")
-
         sdr_confirm_emails = set(sdr_confirm_df['Email'])
-
+        
         # for each row, does the email column value exist in the email list? 
         main_df_copy["SDR 컨펌 여부"] = main_df_copy.apply(
             lambda row: '예' if row["Email"] in sdr_confirm_emails else '', axis=1)
@@ -78,7 +78,7 @@ def main(args):
         main_df_copy[['MKT Review(유효/비유효/홀딩)', 'MKT Review(사유)']] = main_df_copy.apply(
             lambda row: apply_classification(row, step), axis=1, result_type='expand')
    
-    main_df_copy.reset_index(inplace=True, drop=True)
+    # main_df_copy.reset_index(inplace=True, drop=True)
     
     # upload db to excel file 
     upload_db(args, main_df_copy)
